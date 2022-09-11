c2e2a58785a6ba361ea3681983449b8e5a780cca
Dino Pasetti <pasettidino@gmail.com>
2022-09-10 22:05:52 -0300

    Primer commit

---
+++ porcentajes.js
@@ -0,0 +1,36 @@
+function porcentaje (num1, metododepago, tipo){
+    switch(metododepago){
+        case "point":
+            switch(tipo){
+                case "debito":
+                    return (num1*3.15/100);
+
+                case "credito":
+                    return (num1*6.39/100);
+            }
+
+        case "qr":
+            switch(tipo){
+                case "debito":
+                    return (num1*0.99/100);
+
+                case "credito":
+                    return (num1*6.39/100);
+            }
+            
+        case "link":
+            switch(tipo){
+                case "debito":
+                    return (num1*5.99/100);
+
+                case "credito":
+                    return (num1*6.50/100);
+            }
+
+
+        default:
+            return "Operacion no disponible"
+    }
+}
+
+console.log(porcentaje(2500, "qr", "debito"));
