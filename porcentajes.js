41c537fe9293b270d9e6b18706bace091afc05cd
Dino Pasetti <pasettidino@gmail.com>
2022-09-19 18:09:38 -0300

    Se agrego un constructor

--- porcentajes.js
+++ porcentajes.js
@@ -33,5 +33,32 @@ function porcentaje (num1, metododepago, tipo){
     }
 }
 
-/* EJEMPLO */
-console.log(porcentaje(2500, "qr", "debito"));
+class Producto {
+    constructor(id, nombre, precio){
+        this.id =id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+let productos = [];
+productos.push(new Producto(productos.length + 1, "Musculosa Miami Heat", 8000));
+productos.push(new Producto(productos.length + 1, "Short Lakers", 7500));
+productos.push(new Producto(productos.length + 1, "Musculosa Denver Campazzo", 11000));
+productos.push(new Producto(productos.length + 1, "Camiseta seleccion Argentina", 5000));
+productos.push(new Producto(productos.length + 1, "Buzo Cavaliers", 120000));
+console.log(productos);
+
+
+let preciosConInteres = productos.map(producto => {
+    return {
+        nombre: producto.nombre,
+        precio: producto.precio + (porcentaje(productos.precio, "qr", "debito"))
+    };
+})
+
+console.log(preciosConInteres);
+
+/* EJEMPLO *//* 
+console.log(porcentaje(5000, "qr", "debito"));
+ */
